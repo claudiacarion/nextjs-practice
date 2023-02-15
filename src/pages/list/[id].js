3ff694982e19d9e1f0e13bf206fe235dc9680f2a
claudiacarion <claudia.carion@gmail.com>
2023-02-15 16:04:30 +0100

    complete with no styling

--- src/pages/list/[id].js
+++ src/pages/list/[id].js
@@ -1,9 +1,37 @@
 import React from 'react'
 
-const Details = () => {
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/')
+  const data = await res.json()
+
+  const paths = data.map(user => {
+    return {
+      params: { id: user.id.toString() }
+    }
+  })
+  return {
+    paths,
+    fallback: false
+  }
+}
+
+export const getStaticProps = async (context) => {
+  const id = context.params.id
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+  const data = await res.json()
+
+  return {
+    props: { user: data }
+  }
+}
+  
+const Details = ({ user }) => {
   return (
     <div>
       <h1>details</h1>
+      <h3>{user.name}</h3>
+      <p>{user.email}</p>
+      <p>{user.website}</p>
     </div>
   )
 }
