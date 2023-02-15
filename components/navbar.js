6109dbd460cecacfdc50a0f2ce3c1df804ddce62
claudiacarion <claudia.carion@gmail.com>
2023-02-15 15:50:08 +0100

    main pages done

--- components/navbar.js
+++ components/navbar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <nav>
       <div className="logo">
-        <h1>logo</h1>
+        <Image src="/pandalogo.png" alt="logo" width={100} height={100} />
+      </div>
         <Link href="/">home</Link>
         <Link href="/about">about</Link>
-        <Link href="/list/list">list</Link>
-      </div>
+        <Link href="/list">list</Link>
     </nav>
   )
 }
---
+++ src/pages/list/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users')
+  const data = await res.json()
+
+  return {
+    props: { users: data }
+  }
+}
+
+const List = ({ users }) => {
+  return (
+    <div>
+      <h1>list</h1>
+      {users.map(user => (
+        <Link key={user.id} href={`/list/${user.id}`}>
+          <h3>{user.name}</h3>
+          </Link>
+      ))}
+    </div>
+  )
+}
+
+export default List
